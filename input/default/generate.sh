ac3d84b57f4e95efcf23b767a1fa2775dc0171fe
bill K7WXW <91234299+K7WXW@users.noreply.github.com>
2021-12-04 15:46:18 -0800

    Update generate.sh
    put generate.sh in the same state as generate.py

--- input/default/generate.sh
+++ input/default/generate.sh
@@ -7,12 +7,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OUTPUT=${OUTPUT:-$DIR/../../OUTPUT}
 python -m dzcb \
     --pnwdigital \
-    --seattledmr \
-    --default-k7abd \
+#    --seattledmr \
+#    --default-k7abd \
     --farnsworth-template-json $DIR/md380-uhf.json \
                                $DIR/md380-vhf.json \
                                $DIR/md390-uhf.json \
                                $DIR/md390-vhf.json \
-    --scanlists-json $DIR/scanlists.json \
+#    --scanlists-json $DIR/scanlists.json \
     --exclude $DIR/exclude.csv \
 -- $OUTPUT/$(dirname "$DIR")
