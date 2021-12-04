a37cd5bc540169192aca3b34ab55f40683cd85c5
bill K7WXW <91234299+K7WXW@users.noreply.github.com>
2021-12-04 15:22:17 -0800

    update generate.py
    Simplify generate.py to only use pnwdigital and the exclude file for this codeplug.

--- input/default/generate.py
+++ input/default/generate.py
@@ -3,9 +3,11 @@
 # This script generates the same codeplug as generate.sh
 # by running dzcb via the python API
 
-# First round of updates for OR centric codeplug, did not update generate.sh
+# First round of updates for OR centric codeplug,
 # K7WXW 12.03.21
-# Changes to CodePlugRecipe - set seattledmr to False, add prox.csv, add order.csv
+# Changes to CodePlugRecipe - add exclude, comment out seattledmr, default_k7abd,
+# repeaterbook_proximity, scanlists, order.  This should product a codeplug based 
+# only on pnwdigital and the exclude list
 
 from pathlib import Path
 import os
@@ -17,17 +19,13 @@ output = Path(os.environ.get("OUTPUT") or (cp_dir / ".." / ".." / "OUTPUT"))
 
 CodeplugRecipe(
     source_pnwdigital=True,
-# changed, was True
-    source_seattledmr=False,
-    source_default_k7abd=True,
-# added
-    source_k7abd=[(cp_dir / "k7abd")],
-#added
-    source_repeaterbook_proximity=cp_dir / "prox.csv",
-    scanlists_json=cp_dir / "scanlists.json",
+#   source_seattledmr=True,
+#   source_default_k7abd=True,
+#   source_k7abd=[(cp_dir / "k7abd")],
+#   source_repeaterbook_proximity=cp_dir / "prox.csv",
+#   scanlists_json=cp_dir / "scanlists.json",
 #added
     exclude=cp_dir / "exclude.csv",
-#added
-    order=cp_dir / "order.csv",
+#   order=cp_dir / "order.csv",
     output_farnsworth=cp_dir.glob("md3?0-?hf.json"),
 ).generate(output / cp_dir.name)
