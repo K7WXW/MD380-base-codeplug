35fd3a49c89f80e35fb31b532fc546c8ac4ba559
bill K7WXW <91234299+K7WXW@users.noreply.github.com>
2021-12-03 19:52:51 -0800

    Update generate.py
    remodeling generate.py for OR centric codeplug, changes documented in file

--- input/default/K7WXW-md380/generate.py
+++ input/default/K7WXW-md380/generate.py
@@ -3,6 +3,10 @@
 # This script generates the same codeplug as generate.sh
 # by running dzcb via the python API
 
+# First round of updates for OR centric codeplug, did not update generate.sh
+# K7WXW 12.03.21
+# Changes to CodePlugRecipe - set seattledmr to False, add prox.csv, add order.csv
+
 from pathlib import Path
 import os
 
@@ -13,9 +17,17 @@ output = Path(os.environ.get("OUTPUT") or (cp_dir / ".." / ".." / "OUTPUT"))
 
 CodeplugRecipe(
     source_pnwdigital=True,
-    source_seattledmr=True,
+# changed, was True
+    source_seattledmr=False,
     source_default_k7abd=True,
+# added
+    source_k7abd=[(cp_dir / "k7abd")],
+#added
+    source_repeaterbook_proximity=cp_dir / "prox.csv",
     scanlists_json=cp_dir / "scanlists.json",
+#added
     exclude=cp_dir / "exclude.csv",
+#added
+    order=cp_dir / "order.csv",
     output_farnsworth=cp_dir.glob("md3?0-?hf.json"),
 ).generate(output / cp_dir.name)
